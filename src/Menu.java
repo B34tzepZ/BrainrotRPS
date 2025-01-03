0a4377d956e58bc561f62a92cc40de361e657ecd
B34tzepZ <florian.graeber@gmx.de>
2025-01-03 22:23:53 +0100

    Refactoring

--- src/Menu.java
+++ src/Menu.java
@@ -4,31 +4,40 @@ import java.awt.*;
 public class Menu {
     public static void main(String[] args) {
         final JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
+        final JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         frame.add(panel);
-        JLabel shapeLabel = new JLabel("Shape");
+
+        final JLabel shapeLabel = new JLabel("Shape");
         shapeLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
         panel.add(shapeLabel);
+
         final JComboBox<Shape> shape = new JComboBox<>(Shape.values());
         panel.add(shape);
-        final JLabel startingCapacity = new JLabel("Starting Capacity");
-        startingCapacity.setAlignmentX(Component.CENTER_ALIGNMENT);
-        panel.add(startingCapacity);
+
+        final JLabel startingCapacityLabel = new JLabel("Starting Capacity");
+        startingCapacityLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        panel.add(startingCapacityLabel);
+
         final Integer[] startingCapacityOptions = {5, 10, 20, 50};
         final JComboBox<Integer> startingCapacities = new JComboBox<>(startingCapacityOptions);
         panel.add(startingCapacities);
+
         final JLabel maxCapacityLabel = new JLabel("Max Capacity");
         maxCapacityLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
         panel.add(maxCapacityLabel);
+
         final JCheckBox useStartingCapacity = new JCheckBox("Same as Starting Capacity");
         useStartingCapacity.setAlignmentX(Component.CENTER_ALIGNMENT);
         panel.add(useStartingCapacity);
+
         final JSpinner spinner = new JSpinner();
         panel.add(spinner);
+
         final JButton button = new JButton("GO");
         button.setAlignmentX(Component.CENTER_ALIGNMENT);
         panel.add(button);
+
         button.addActionListener(e -> {
             int maxCapacity = useStartingCapacity.isSelected() ? (int) startingCapacities.getSelectedItem() : (int) spinner.getValue();
             JFrame gameWindow = DisplayArea.newWindow((Shape) shape.getSelectedItem(), (int) startingCapacities.getSelectedItem(), maxCapacity);
